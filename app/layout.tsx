dd1bdd9f4fb11b3427ca49fa47e3a1778446c3af
Dahiru Usman Hassan <usmandahirub@gmail.com>
2026-04-09 14:27:05 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "idcode",
   description: "",
+  icons: {
+    icon: "icon.jpeg", 
+  },
 };
 
 export default function RootLayout({
